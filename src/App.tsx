4f8cc7a1bca197ea69cb6d8a3a8bfb6dce58c148
ApTeM <fleperartem@gmail.com>
2023-07-31 11:10:09 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import routerBindings, {
   DocumentTitleHandler,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
+import { HeadlessInferencer } from "@refinedev/inferencer/headless";
 import { Layout } from "./components/layout";
 import "./App.css";
 
@@ -24,22 +25,51 @@ function App() {
     <BrowserRouter>
       <GitHubBanner />
       <RefineKbarProvider>
-        <Refine
-          routerProvider={routerBindings}
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-          options={{
-            syncWithLocation: true,
-            warnWhenUnsavedChanges: true,
-            projectId: "UxARWe-OXeKmD-t6LkBZ",
-          }}
-        >
-          <Routes>
-            <Route index element={<WelcomePage />} />
-          </Routes>
-          <RefineKbar />
-          <UnsavedChangesNotifier />
-          <DocumentTitleHandler />
-        </Refine>
+      <Refine
+                routerProvider={routerBindings}
+                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                resources={[
+                    {
+                        name: "blog_posts",
+                        list: "/blog-posts",
+                        show: "/blog-posts/show/:id",
+                        create: "/blog-posts/create",
+                        edit: "/blog-posts/edit/:id",
+                    },
+                ]}
+                options={{
+                    syncWithLocation: true,
+                    warnWhenUnsavedChanges: true,
+                }}
+            >
+                <Routes>
+                    <Route
+                        element={
+                            <Layout>
+                                <Outlet />
+                            </Layout>
+                        }
+                    >
+                        <Route index element={<NavigateToResource resource="blog_posts" />} />
+                        <Route path="blog-posts">
+                            <Route index element={<HeadlessInferencer />} />
+                            <Route
+                                path="show/:id"
+                                element={<HeadlessInferencer />}
+                            />
+                            <Route
+                                path="edit/:id"
+                                element={<HeadlessInferencer />}
+                            />
+                            <Route
+                                path="create"
+                                element={<HeadlessInferencer />}
+                            />
+                        </Route>
+                    </Route>
+                </Routes>
+                <UnsavedChangesNotifier />
+            </Refine>
       </RefineKbarProvider>
     </BrowserRouter>
   );
